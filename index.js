7cf0b715b3ccefceb89605c01bc98a702dc09c40
unknown <filip.trifunovic23@gmail.com>
2023-04-24 13:30:29 +0200

    adding helper function

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const model = {
   boardSize: 7,
   numShips: 3,
   shipLength: 3,
+  shipSunk: 0,
   ships: [
     {
       locations: ["06", "16", "26"],
@@ -33,4 +34,69 @@ const model = {
       hits: ["", "", ""],
     },
   ],
+  fire: function (guess) {
+    for (let i = 0; i < this.numShips; i++) {
+      const ship = this.ships[i];
+      const locations = ship.locations;
+      const index = locations.indexOf(guess);
+
+      if (index >= 0) {
+        // pogodak
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT");
+
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipSunk++;
+        }
+
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed!");
+    return false;
+  },
+  isSunk: function (ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  },
 };
+
+// CONTROLER objekat, zaduzen je da spoji delove aplikacije
+const controller = {
+  guesses: 0,
+  processGuess: function (guess) {},
+};
+
+// pomocna funkcija koja ce proveriti da li je input od igraca validan
+function parseGuess(guess) {
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("Please enter letter and number on the board!");
+  } else {
+    const firstChar = guess.charAt(0);
+    const row = alphabet.indexOf(firstChar);
+    const column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("That isn't on board!");
+    } else if (
+      row < 0 ||
+      row >= model.boardSize ||
+      column < 0 ||
+      column >= model.boardSize
+    ) {
+      alert("Thats off the board!");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
